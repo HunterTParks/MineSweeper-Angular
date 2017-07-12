339d8a49cc101e4ee2aab61246fa476cee0e4a93
Hunter Parks and Pete Lazuran <hunter+pete@>
2017-07-12 16:33:34 -0700

    add stuff

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Tile } from '../tile.model';
 import { Minefield } from '../minefield.model';
+import { minesweeper } from 'minesweeper';
 
 @Component({
   selector: 'app-grid',
@@ -14,20 +15,58 @@ export class GridComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.minefield);
     for(var i = 0; i < 12; i ++){
       var rows = [];
       for(var ii = 0; ii < 12; ii ++){
         var newTile: Tile = new Tile();
+        var rng = Math.floor(Math.random() * 10) + 1;
+        if(rng > 6){
+          newTile.bomb = true;
+        }
         rows.push(newTile);
-        // console.log(rows);
       }
-
       this.minefield.columns.push(rows);
-
     }
-    // console.log(this.minefield.columns);
-    // console.log(rows);
+    console.log(this.minefield);
+    for(var j = 0; j <= 12; j++){
+      for(var jj = 0; jj <= 12; jj++){
+        if(this.minefield.columns[j][jj].bomb === true){
+          if(this.minefield.columns[j - 1] === undefined || this.minefield.columns[j - 1][jj - 1] === undefined){
+          } else {
+            this.minefield.columns[j - 1][jj - 1].count++;
+          }
+          if(this.minefield.columns[j - 1] === undefined){
+          } else {
+            this.minefield.columns[j - 1][jj].count++;
+          }
+          if (this.minefield.columns[j - 1] === undefined || this.minefield.columns[j - 1][jj +1] === undefined){
+          } else {
+            this.minefield.columns[j - 1][jj + 1].count++;
+          }
+          if (this.minefield.columns[j][jj - 1] === undefined){
+          }else {
+            this.minefield.columns[j][jj - 1].count++;
+          }
+          if (this.minefield.columns[j][jj + 1] === undefined) {
+          } else {
+            this.minefield.columns[j][jj + 1].count++;
+          }
+          if (this.minefield.columns[j + 1] === undefined || this.minefield.columns[j + 1][jj - 1] === undefined) {
+          } else {
+            this.minefield.columns[j + 1][jj - 1].count++;
+          }
+          if (this.minefield.columns[j + 1] === undefined) {
+          } else {
+            this.minefield.columns[j + 1][jj].count++;
+          }
+          if (this.minefield.columns[j + 1] === undefined || this.minefield.columns[j + 1][jj + 1] === undefined){
+          }else {
+            this.minefield.columns[j + 1][jj + 1].count++;
+          }
+
+        }
+      }
+    }
   }
 
   onClick(tile: Tile){
